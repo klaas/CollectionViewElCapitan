e6933a2a58c0842076d726e56e96526edeb83d6e
Klaas <Klaasmer@gmail.com>
2015-10-07 11:51:06 +0200

    Initial Commit

---
+++ CollectionViewElCapitan/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CollectionViewElCapitan
+//
+//  Created by Klaas on 07.10.15.
+//  Copyright © 2015 Park Bench. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ CollectionViewElCapitan/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CollectionViewElCapitan
+//
+//  Created by Klaas on 07.10.15.
+//  Copyright © 2015 Park Bench. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
